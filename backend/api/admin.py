aae0ee0852d628017df7ce63fa7d3ea21c473f45
Ashish <aashishdhakal71@gmail.com>
2024-09-08 23:21:06 +0545

    Initial commit with backend and frontend

---
+++ backend/api/admin.py
@@ -0,0 +1,9 @@
+from django.contrib import admin
+from api import models as api_models
+admin.site.register(api_models.User)
+admin.site.register(api_models.Profile)
+admin.site.register(api_models.Category)
+admin.site.register(api_models.Blog)
+admin.site.register(api_models.Comment)
+admin.site.register(api_models.Bookmark)
+admin.site.register(api_models.Notification)
